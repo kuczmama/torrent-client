e79133a9d41fb6ec7d10696601642ba25a702941
Mark Kuczmarski <mark.kuczmarski@sbisecsol.com>
2020-08-03 17:04:37 +0900

    Add ability to see list of peers

---
+++ src/tracker.js
@@ -0,0 +1,132 @@
+'use strict';
+
+const dgram = require('dgram');
+const Buffer = require('buffer').Buffer;
+const urlParse = require('url').parse;
+const crypto = require('crypto');
+const torrentParser = require('./torrent-parser');
+const util = require('./util');
+
+module.exports.getPeers = (torrent, callback) => {
+  const socket = dgram.createSocket('udp4');
+  const url = torrent.announce.toString('utf8');
+
+  // 1. send connect request
+  udpSend(socket, buildConnReq(), url);
+
+  socket.on('message', response => {
+    if (respType(response) === 'connect') {
+      // 2. receive and parse connect response
+      const connResp = parseConnResp(response);
+      console.log("connResp", connResp);
+      // 3. send announce request
+      const announceReq = buildAnnounceReq(connResp.connectionId, torrent);
+      udpSend(socket, announceReq, url);
+    } else if (respType(response) === 'announce') {
+      // 4. parse announce response
+      const announceResp = parseAnnounceResp(response);
+      // 5. pass peers to callback
+      callback(announceResp.peers);
+    }
+  });
+};
+
+function udpSend(socket, message, rawUrl, callback=()=>{}) {
+  const url = urlParse(rawUrl);
+  socket.send(message, 0, message.length, url.port, url.hostname, callback);
+}
+
+function respType(resp) {
+  const action = resp.readUInt32BE(0);
+  if (action === 0) return 'connect';
+  if (action === 1) return 'announce';
+}
+
+function buildConnReq() {
+  const buf = Buffer.alloc(16); // 2
+
+  // connection id
+  buf.writeUInt32BE(0x417, 0); // 3
+  buf.writeUInt32BE(0x27101980, 4);
+  // action
+  buf.writeUInt32BE(0, 8); // 4
+  // transaction id
+  crypto.randomBytes(4).copy(buf, 12); // 5
+
+  return buf;
+}
+
+function parseConnResp(resp) {
+  return {
+    action: resp.readUInt32BE(0),
+    transactionId: resp.readUInt32BE(4),
+    connectionId: resp.slice(8)
+  }
+}
+
+function buildAnnounceReq(connId, torrent, port=6881) {
+  const buf = Buffer.allocUnsafe(98);
+  // connection id
+  connId.copy(buf, 0);
+  // action
+  buf.writeUInt32BE(1, 8);
+  // transaction id
+  crypto.randomBytes(4).copy(buf, 12);
+  // info hash
+  torrentParser.infoHash(torrent).copy(buf, 16);
+  // peerId
+  util.genId().copy(buf, 36);
+  // downloaded
+  Buffer.alloc(8).copy(buf, 56);
+  // left
+  torrentParser.size(torrent).copy(buf, 64);
+  // uploaded
+  Buffer.alloc(8).copy(buf, 72);
+  // event
+  buf.writeUInt32BE(0, 80);
+  // ip address
+  buf.writeUInt32BE(0, 80);
+  // key
+  crypto.randomBytes(4).copy(buf, 88);
+  // num want
+  buf.writeInt32BE(-1, 92);
+  // port
+  buf.writeUInt16BE(port, 96);
+
+  return buf;
+}
+
+// Offset      Size            Name            Value
+// 0           32-bit integer  action          1 // announce
+// 4           32-bit integer  transaction_id
+// 8           32-bit integer  interval
+// 12          32-bit integer  leechers
+// 16          32-bit integer  seeders
+// 20 + 6 * n  32-bit integer  IP address
+// 24 + 6 * n  16-bit integer  TCP port
+// 20 + 6 * N
+function parseAnnounceResp(resp) {
+  const GROUP_SIZE = 6;
+
+  const group = (iterable) => {
+    let groups = [];
+    for (let i = 0; i < iterable.length; i += GROUP_SIZE) {
+      groups.push(iterable.slice(i, i + GROUP_SIZE));
+    }
+    return groups;
+  }
+
+  return {
+    action: resp.readUInt32BE(0),
+    transactionId: resp.readUInt32BE(4),
+    interval: resp.readUInt32BE(8),
+    leechers: resp.readUInt32BE(12),
+    seeders: resp.readUInt32BE(16),
+    peers: group(resp.slice(20)).map(address => {
+      return {
+        ip: address.slice(0, 4).join('.'),
+        port: address.readUInt16BE(4)
+      }
+    })
+  }
+}
