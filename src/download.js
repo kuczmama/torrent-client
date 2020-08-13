50bfd0b158f56a768c68778bc4d29caf965dcdc1
Mark Kuczmarski <mark@mkucz.com>
2020-08-13 11:03:26 +0900

    Add non-handshake message handlers

--- src/download.js
+++ src/download.js
@@ -3,21 +3,97 @@
 const net = require("net");
 const Buffer = require("buffer").Buffer;
 const tracker = require("./tracker");
+const message = require("./message");
 
 module.exports = (torrent) => {
   tracker.getPeers(torrent, (peers) => {
-    peers.map((peer) => console.log(peer));
-    peers.forEach(download);
+    peers.map((peer) => download(peer, torrent));
   });
 };
 
-const download = (peer) => {
+const download = (peer, torrent) => {
+  console.log("download", peer, torrent);
   const socket = net.Socket();
   socket.on("error", console.log);
   socket.connect(peer.port, peer.ip, () => {
-    // socket.write(...) write a message here
+    // 1
+    socket.write(message.buildHandshake(torrent));
   });
-  socket.on("data", (data) => {
-    // handle response here
+  // 2
+  onWholeMsg(socket, (msg) => {
+    console.log("Got message", msg);
+    msgHandler(msg, socket);
+  });
+};
+
+// 2
+const msgHandler = (msg, socket) => {
+  if (isHandshake(msg)) {
+    socket.write(message.buildInterested());
+  } else {
+    const m = message.parse(msg);
+
+    switch (m.id) {
+      case 0:
+        chokeHandler();
+        break;
+      case 1:
+        unchokeHandler();
+        break;
+      case 4:
+        haveHandler(m.payload);
+        break;
+      case 5:
+        bitfieldHandler(m.payload);
+        break;
+      case 7:
+        pieceHandler(m.payload);
+        break;
+    }
+  }
+};
+
+chokeHandler = () => {
+  //...
+};
+
+unchokeHandler = () => {
+  //...
+};
+
+haveHandler = (payload) => {
+  //...
+};
+
+bitfieldHandler = (payload) => {
+  //...
+};
+
+pieceHandler = (payload) => {
+  //...
+};
+
+// 3
+const isHandshake = (msg) => {
+  return (
+    msg.length === msg.readUInt8(0) + 49 &&
+    msg.toString("utf8", 1) === "BitTorrent protocol"
+  );
+};
+
+const onWholeMsg = (socket, callback) => {
+  let savedBuf = Buffer.alloc(0);
+  let handshake = true;
+  socket.on("data", (recvBuf) => {
+    // msgLen calculated the length of a whole message
+    const msgLen = () =>
+      handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+    savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+    while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+      callback(savedBuf.slice(msgLen()));
+      savedBuf = savedBuf.slice(msgLen());
+      handshake = false;
+    }
   });
 };
--- src/message.js
+++ src/message.js
@@ -2,6 +2,7 @@
 
 const Buffer = require("buffer").Buffer;
 const torrentParser = require("./torrent-parser");
+const util = require("./util");
 
 // handshake: <pstrlen><pstr><reserved><info_hash><peer_id>
 
@@ -13,7 +14,7 @@ const torrentParser = require("./torrent-parser");
 //   In version 1.0 of the BitTorrent protocol, pstrlen = 19, and pstr = "BitTorrent protocol".
 
 module.exports.buildHandshake = (torrent) => {
-  const buf = buffer.alloc(68);
+  const buf = Buffer.alloc(68);
   // pstrlen
   buf.writeUInt8(19, 0);
   // pstr
@@ -24,7 +25,7 @@ module.exports.buildHandshake = (torrent) => {
   // info hash
   torrentParser.infoHash(torrent).copy(buf, 28);
   // peer id
-  buf.write(util.genId());
+  util.genId().copy(buf, 48);
   return buf;
 };
 
@@ -214,4 +215,24 @@ module.exports.buildPort = (payload) => {
   // listen-port
   buf.writeUInt16BE(payload, 5);
   return buf;
+};
+
+module.exports.parse = (msg) => {
+  const id = msg.length > 4 ? msg.readInt8(4) : null;
+  let payload = msg.length > 5 ? msg.slice(5) : null;
+
+  if (id === 6 || id === 7 || id === 8) {
+    const rest = payload.slice(8);
+    payload = {
+      index: payload.readInt32BE(0),
+      begin: payload.readInt32BE(4),
+    };
+    payload[id === 7 ? "block" : "length"] = rest;
+  }
+
+  return {
+    size: msg.readInt32BE(0),
+    id: id,
+    payload: payload,
+  };
 };
